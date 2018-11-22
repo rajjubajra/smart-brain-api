d38a1962c21e39d54d924b775157d747c9d4b0d8
rajju <vajrachara@yahoo.com>
2018-11-22 10:51:54 +0000

    get path updated

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ database = {
   ]
 }
 
-// app.get('/',(req, res) => {res.json(database.users);} )
+
 app.get('/',(req, res) => {res.send("it is working")} )
 
 app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt)})
